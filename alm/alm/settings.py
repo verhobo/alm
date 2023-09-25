82d6eee516be2b9c1da738405bfa9ecaa46da471
verhobo <verocityhobo@gmail.com>
2023-09-25 19:31:58 -0400

    production

--- alm/alm/settings.py
+++ alm/alm/settings.py
@@ -21,10 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1qwhb^w-0vz%*j=guogl6q848eavyp3$)#e#1ns&gc#6dig530'
+#SECRET_KEY = 'django-insecure-1qwhb^w-0vz%*j=guogl6q848eavyp3$)#e#1ns&gc#6dig530'
+
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SECURE_REFERRER_POLICY = "same-origin"
 
 ALLOWED_HOSTS = ["104.131.166.109", "localhost", "www.almhomeschool.com", "almhomeschool.com"]
 
@@ -121,8 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
